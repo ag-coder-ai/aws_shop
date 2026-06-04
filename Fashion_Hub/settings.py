f887dc41f3a934567d5ce9225baa43664c3e6d5e
vasu-techno <vasu@techno-forest.com>
2026-06-05 02:20:46 +0530

    deploy

--- Fashion_Hub/settings.py
+++ Fashion_Hub/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'Fashion_Hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'unitythreads_db',
+        'USER': 'postgres',
+        'PASSWORD': '779969',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Fashion_Hub.wsgi:application
