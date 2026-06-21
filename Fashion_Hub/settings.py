6696f7447bfb918320a5965350fd46a44b64f99a
vasu-techno <vasu@techno-forest.com>
2026-06-21 21:23:17 +0530

    staicfile configuration

--- Fashion_Hub/settings.py
+++ Fashion_Hub/settings.py
@@ -145,7 +145,7 @@ LOGIN_URL = "login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / "staticfiles"
